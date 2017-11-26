70e10482716eb7c43c8ef62f9df72b4301b9a058
wudizhuo <sunzhuo1228@126.com>
2017-11-25 20:43:44 -0800

    refactor code

--- src/test/kotlin/com/sunzhuo/kindle/module/sender/utils/HtmlExtractTest.kt
+++ src/test/kotlin/com/sunzhuo/kindle/module/sender/utils/HtmlExtractTest.kt
@@ -9,20 +9,19 @@ class HtmlExtractTest {
         val readabilityHtml = HtmlExtract.getReadabilityHtml("http://www.jianshu.com/p/e815b9e9dd6d?hmsr=toutiao.io&utm_medium=toutiao.io&utm_source=toutiao.io")
         println(readabilityHtml.content)
         assertTrue(readabilityHtml.length > 0)
-        //https://mp.weixin.qq.com/s/_8_-y7G8NGUj5WeOk3oaMQ
     }
 
     @test
     fun testWeiXin() {
         val readabilityHtml = HtmlExtract.getReadabilityHtml("https://mp.weixin.qq.com/s/_8_-y7G8NGUj5WeOk3oaMQ")
-        println(readabilityHtml.content)
+//        println(readabilityHtml.content)
         assertTrue(readabilityHtml.length > 0)
     }
 
     @test
     fun testZhihu() {
         val readabilityHtml = HtmlExtract.getReadabilityHtml("https://zhuanlan.zhihu.com/p/22049205")
-        println(readabilityHtml.content)
+//        println(readabilityHtml.content)
         assertTrue(readabilityHtml.length > 0)
     }
 }
