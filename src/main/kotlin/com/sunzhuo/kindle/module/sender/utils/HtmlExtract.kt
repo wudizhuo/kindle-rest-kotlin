901a4f98ce4afd690bab9e549cd11e0e847c830d
wudizhuo <sunzhuo1228@126.com>
2017-12-03 10:23:07 -0800

    add TODO

--- src/main/kotlin/com/sunzhuo/kindle/module/sender/utils/HtmlExtract.kt
+++ src/main/kotlin/com/sunzhuo/kindle/module/sender/utils/HtmlExtract.kt
@@ -40,6 +40,7 @@ object HtmlExtract {
     fun getReadabilityHtmlAndSave2Local(url: String): String {
         getArticleJson(url)
 
+        //TODO 这里没有错误的话 就可以先返回ok 再继续后台做就可以了，不需要前台等这么久
         //TODO 用子线程 继续做
 
         var regex = "<img[\\s\\S]*?src\\s*=\\s*[\\\"|'](.*?)[\\\"|'][\\s\\S]*?>"
