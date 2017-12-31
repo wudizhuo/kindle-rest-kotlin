7c99639e8b589090127ead4b1c8be28caa0640d4
wudizhuo <sunzhuo1228@126.com>
2017-12-31 12:02:22 -0800

    update regex

--- src/main/kotlin/com/sunzhuo/kindle/module/sender/utils/HtmlExtract.kt
+++ src/main/kotlin/com/sunzhuo/kindle/module/sender/utils/HtmlExtract.kt
@@ -42,7 +42,7 @@ class HtmlExtract {
         //TODO 这里没有错误的话 就可以先返回ok 再继续后台做就可以了，不需要前台等这么久
         //TODO 用子线程 继续做
 
-        var regex = "<img[\\s\\S]*?src\\s*=\\s*[\\\"|'](.*?)[\\\"|'][\\s\\S]*?>"
+        var regex = """<img[\s\S]*?src\s*=\s*["|'](.*?)["|'][\s\S]*?>"""
         var pageSource = driver.pageSource
 
         val m = Pattern.compile(regex).matcher(pageSource)
