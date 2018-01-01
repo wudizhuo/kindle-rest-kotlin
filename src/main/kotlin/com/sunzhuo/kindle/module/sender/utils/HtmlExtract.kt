d5c7f46052dca7eaea508450f1dbdb0e56007adb
wudizhuo <sunzhuo1228@126.com>
2017-12-31 22:43:28 -0800

    refactor driver code

--- src/main/kotlin/com/sunzhuo/kindle/module/sender/utils/HtmlExtract.kt
+++ src/main/kotlin/com/sunzhuo/kindle/module/sender/utils/HtmlExtract.kt
@@ -5,10 +5,6 @@ import com.sunzhuo.kindle.common.httpstatus.UrlContentNotFoundException
 import com.sunzhuo.kindle.module.sender.domain.Article
 import org.openqa.selenium.WebDriverException
 import org.openqa.selenium.phantomjs.PhantomJSDriver
-import org.openqa.selenium.phantomjs.PhantomJSDriverService
-import org.openqa.selenium.remote.CapabilityType
-import org.openqa.selenium.remote.DesiredCapabilities
-import org.openqa.selenium.remote.HttpCommandExecutor
 import org.springframework.boot.ApplicationTemp
 import org.springframework.core.io.ClassPathResource
 import java.io.File
@@ -17,21 +13,8 @@ import java.util.regex.Pattern
 import javax.imageio.ImageIO
 
 class HtmlExtract {
-    val driver: PhantomJSDriver
+    val driver: PhantomJSDriver = DriverProvider.getDrive()
     val imgTagRegex = """<img[\s\S]*?src\s*=\s*\s*["|'](http.*?)["|'][\s\S]*?>"""
-
-    init {
-        val phantomjs = DesiredCapabilities.phantomjs()
-        phantomjs.isJavascriptEnabled = true
-        phantomjs.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true)
-        phantomjs.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, arrayOf("--ssl-protocol=tlsv1", "--ignore-ssl-errors=yes", "--load-images=no"))
-        val userAgent = """Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.114 Safari/537.36"""
-        phantomjs.setCapability("phantomjs.page.settings.userAgent", userAgent)
-
-        driver = PhantomJSDriver(
-                HttpCommandExecutor(URL("http://127.0.0.1:8910")),
-                phantomjs)
-    }
 
     fun getReadabilityHtml(url: String): Article {
         val articleJson: String = getArticleJson(url)
