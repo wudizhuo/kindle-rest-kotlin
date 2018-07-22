147d84828dc3cc5a39ced2fac48989e76f591e47
wudizhuo <sunzhuo1228@126.com>
2018-07-21 17:19:41 -0700

    add configurer

---
+++ src/main/kotlin/com/sunzhuo/kindle/cofig/AppWebMvcConfigurer.kt
@@ -0,0 +1,13 @@
+package com.sunzhuo.kindle.cofig
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@EnableWebMvc
+class AppWebMvcConfigurer: WebMvcConfigurer{
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://www.kindlezhushou.com", "http://kindlezhushou.com")
+    }
+}
---
+++ src/main/kotlin/com/sunzhuo/kindle/cofig/Application.kt
@@ -0,0 +1,21 @@
+package com.sunzhuo.kindle.cofig
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@SpringBootApplication
+class Application {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            SpringApplication.run(Application::class.java, *args)
+        }
+    }
+
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return AppWebMvcConfigurer()
+    }
+}
