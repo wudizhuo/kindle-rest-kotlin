3aa9aa9fb8660a247893143ca0795ba131782272
wudizhuo <sunzhuo1228@126.com>
2018-07-22 23:50:30 -0700

    add kotlin readability lib instead of jsEngine

--- src/test/kotlin/com/sunzhuo/kindle/module/sender/utils/HtmlExtractTest.kt
+++ src/test/kotlin/com/sunzhuo/kindle/module/sender/utils/HtmlExtractTest.kt
@@ -7,13 +7,6 @@ import org.junit.Test as test
 
 class HtmlExtractTest {
     @test
-    fun testGetContentByMozillaLib() {
-        val readabilityHtml = HtmlExtract().getReadabilityHtml("http://www.jianshu.com/p/e815b9e9dd6d?hmsr=toutiao.io&utm_medium=toutiao.io&utm_source=toutiao.io")
-        println(readabilityHtml.content)
-        assertTrue(readabilityHtml.length > 0)
-    }
-
-    @test
     fun testWeiXin() {
         val readabilityHtml = HtmlExtract().getReadabilityHtml("https://mp.weixin.qq.com/s/_8_-y7G8NGUj5WeOk3oaMQ")
         println(readabilityHtml.content)
