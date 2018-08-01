48d15fd3189b3bfbfc58f1d83c7d5d7a7b725593
wudizhuo <sunzhuo1228@126.com>
2018-07-31 22:27:14 -0700

    refactor

--- src/main/java/com/sunzhuo/kindle/config/Application.kt
+++ src/main/java/com/sunzhuo/kindle/config/Application.kt
@@ -1,22 +1,20 @@
 package com.sunzhuo.kindle.config
 
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
-@SpringBootApplication (exclude = [(DataSourceAutoConfiguration::class)])
+@SpringBootApplication(exclude = [(DataSourceAutoConfiguration::class)])
 class Application {
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            SpringApplication.run(Application::class.java, *args)
-        }
-    }
 
     @Bean
     fun corsConfigurer(): WebMvcConfigurer {
         return AppWebMvcConfigurer()
     }
+}
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
 }
