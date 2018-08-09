4e173d7b9d101cee3f7a67f800b27800fecbd6fd
wudizhuo <sunzhuo1228@126.com>
2018-08-09 00:38:56 -0700

    fix bug

--- src/main/kotlin/com/sunzhuo/kindle/AppWebMvcConfigurer.kt
+++ src/main/kotlin/com/sunzhuo/kindle/AppWebMvcConfigurer.kt
@@ -1,31 +1,10 @@
 package com.sunzhuo.kindle
 
-import com.moesif.servlet.MoesifFilter
 import com.sunzhuo.kindle.module.sender.utils.LoggerReport
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.servlet.ModelAndView
-import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-import javax.servlet.Filter
 import javax.servlet.http.HttpServletRequest
-
-@Configuration
-@EnableWebMvc
-class AppWebMvcConfigurer: WebMvcConfigurer{
-    override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://www.kindlezhushou.com", "http://kindlezhushou.com")
-    }
-
-    @Bean
-    fun moesifFilter(): Filter {
-        return MoesifFilter("eyJhcHAiOiIxNTA6MjEwIiwidmVyIjoiMi4wIiwib3JnIjoiMjQwOjE4NCIsImlhdCI6MTUzMzYwMDAwMH0.xd2QXMfLVa3ph0fd9ZaQKko_2ZXV8xtmHca7W_QJBAY")
-    }
-}
 
 @ControllerAdvice
 class GlobalDefaultExceptionHandler {
--- src/main/kotlin/com/sunzhuo/kindle/Application.kt
+++ src/main/kotlin/com/sunzhuo/kindle/Application.kt
@@ -1,16 +1,30 @@
 package com.sunzhuo.kindle
 
+import com.moesif.servlet.MoesifFilter
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import javax.servlet.Filter
+
 
 @SpringBootApplication
 class Application {
 
     @Bean
     fun corsConfigurer(): WebMvcConfigurer {
-        return AppWebMvcConfigurer()
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://www.kindlezhushou.com", "http://kindlezhushou.com")
+            }
+        }
+    }
+
+    @Bean
+    fun moesifFilter(): Filter {
+        return MoesifFilter("eyJhcHAiOiIxNTA6MjEwIiwidmVyIjoiMi4wIiwib3JnIjoiMjQwOjE4NCIsImlhdCI6MTUzMzYwMDAwMH0.xd2QXMfLVa3ph0fd9ZaQKko_2ZXV8xtmHca7W_QJBAY")
     }
 }
 
